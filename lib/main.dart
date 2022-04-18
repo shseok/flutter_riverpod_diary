6678c30cc2d4bb88bed44398a3587e7620ca3652
shseok <gus0674@ajou.ac.kr>
2022-04-19 08:09:40 +0900

    Create Custom Floating Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart'; // 기본 내장
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 // 등록 후 내장
 void main() => runApp(MyApp());
@@ -15,8 +16,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
+  var rmicons = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +33,65 @@ class MyPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('initial page'),
         centerTitle: true,
+      ),
+      floatingActionButton: SpeedDial(
+        backgroundColor: Color(0xffA876DE),
+        // add 0xff to front
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        spacing: 3,
+        openCloseDial: isDialOpen,
+        buttonSize: const Size(56.0, 56.0),
+        childrenButtonSize: const Size(56.0, 56.0),
+        visible: true,
+        direction: SpeedDialDirection.up,
+        renderOverlay: true,
+        onOpen: () => debugPrint('OPENING DIAL'),
+        onClose: () => debugPrint('DIAL CLOSED'),
+        elevation: 8.0,
+        isOpenOnStart: false,
+        animationSpeed: 150,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        children: [
+          SpeedDialChild(
+            child: !rmicons
+                ? const Icon(
+                    Icons.post_add,
+                  )
+                : null,
+            backgroundColor: Colors.white,
+            // foregroundColor: Colors.white,
+            label: '추가',
+            onTap: () => setState(() => rmicons = !rmicons),
+            onLongPress: () => debugPrint('FIRST CHILD LONG PRESS'),
+          ),
+          SpeedDialChild(
+            child: !rmicons
+                ? const Icon(
+                    Icons.delete_outline,
+                  )
+                : null,
+            backgroundColor: Colors.white,
+            // foregroundColor: Colors.white,
+            label: '삭제',
+            onTap: () => debugPrint('SECOND CHILD'),
+          ),
+          SpeedDialChild(
+            child: !rmicons
+                ? const Icon(
+                    Icons.edit_note,
+                  )
+                : null,
+            backgroundColor: Colors.white,
+            // foregroundColor: Colors.white,
+            label: '수정',
+            visible: true,
+            onTap: () => ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text(("Third Child Pressed")))),
+            onLongPress: () => debugPrint('THIRD CHILD LONG PRESS'),
+          ),
+        ],
       ),
     );
   }
