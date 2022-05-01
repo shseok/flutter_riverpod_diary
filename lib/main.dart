e2a808995a7bc9838f25f438107f99f66630b886
shseok <gus0674@ajou.ac.kr>
2022-05-01 20:55:57 +0900

    Not Plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart'; // 기본 내장
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 // 등록 후 내장
 void main() => runApp(MyApp());
 
@@ -10,7 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'initial page',
+      theme: ThemeData(primaryColor: Color(0xffA876DE)),
       home: MyPage(),
+      localizationsDelegates: [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('ko', 'KR')],
     );
   }
 }
@@ -21,10 +29,37 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('initial page'),
-        centerTitle: true,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('initial page'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: Text('클릭'),
+            onPressed: () {
+              showDatePicker(
+                  initialEntryMode : DatePickerEntryMode.calendarOnly,
+                  context: context,
+                  helpText: '',
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2010),
+                  lastDate: DateTime(2025),
+                  builder: (context, child) =>
+                      Theme(
+                        data: ThemeData().copyWith(
+                          colorScheme: ColorScheme.light(
+                            primary: Color(0xffA876DE),
+                            onPrimary: Colors.white,
+                            surface: Color(0xffA876DE),
+                            onSurface: Colors.black,
+                          ),
+                          dialogTheme: DialogTheme(),
+                        ),
+                        child: child!,
+                      )
+              );
+            },
+          ),
+        ));
   }
 }
